85f1aad2bcce3ce87cd2a344d27b63433993cb64
Young Wang <1099216788@qq.com>
2022-08-02 10:35:29 +0800

    Update index.ts

--- src/modules/service/axios/index.ts
+++ src/modules/service/axios/index.ts
@@ -16,7 +16,7 @@ const axiosInstance = axios.create({
   withCredentials: false, // 表示跨域请求时是否需要使用凭证, 默认false
   headers: {
     'Content-Type': 'application/json;charset=UTF-8',
-    'Authorization': localStorage.getItem('token') || ''
+    'Authorization': 'Bearer ' + localStorage.getItem('token') || ''
   },
 })
 
