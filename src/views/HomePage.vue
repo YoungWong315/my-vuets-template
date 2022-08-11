8ac7354dd6ddcb65711377a926027b2311d49035
Young Wang <1099216788@qq.com>
2022-08-11 16:45:47 +0800

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 
-import { Auth, updateToken, ResponseCode, TypeUser } from '@/modules/service'
+import { Auth, updateToken, ResponseCode } from '@/modules/service'
 
 console.log(ResponseCode)
 </script>
